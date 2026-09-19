1421288e052ede5de5d257d5bd06cfda6b872876
LEOGO-arch <leogodigitalmarket2@gmail.com>
2026-09-19 22:18:19 +0300

    Complete Seller taxonomy with Bookshop garments vehicle and Other specify

--- partner/taxonomy.js
+++ partner/taxonomy.js
@@ -1,5 +1,5 @@
 window.LEOGO_PRODUCT_TAXONOMY = {
-  version: '2026-09-19.3',
+  version: '2026-09-19.4',
   categories: [
     {id:'184a9f5f-8870-4112-87e4-d9c8d4c63a5e',code:'food_drinks',name:'FOOD & DRINKS',display_order:10,is_active:true,is_assignable:true,restricted_category:false},
     {id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'groceries',name:'GROCERIES',display_order:20,is_active:true,is_assignable:true,restricted_category:false},
@@ -8,21 +8,22 @@ window.LEOGO_PRODUCT_TAXONOMY = {
     {id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'bookshop',name:'BOOKSHOP',display_order:50,is_active:true,is_assignable:true,restricted_category:false},
     {id:'15774c65-7253-42e6-97fd-8fa46af43d4c',code:'hardware',name:'HARDWARE',display_order:60,is_active:true,is_assignable:true,restricted_category:false},
     {id:'a7674f9c-0579-42f0-90a5-1ee51fc9727e',code:'second_hand_market',name:'SECOND HAND MARKET',display_order:80,is_active:true,is_assignable:true,restricted_category:false},
-    {id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'garments_clothes_footwear',name:'GARMENTS, CLOTHES & FOOTWEARS',display_order:90,is_active:true,is_assignable:true,restricted_category:false},
+    {id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'garments_clothes_footwear',name:'GARMENTS, CLOTHES,FOOTWARES',display_order:90,is_active:true,is_assignable:true,restricted_category:false},
     {id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'jewelry',name:'JEWELRIES',display_order:100,is_active:true,is_assignable:true,restricted_category:false},
     {id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'alcoholic_leogo_bar',name:'ALCOHOLIC (LEOGO DIGITAL BAR)',display_order:110,is_active:true,is_assignable:true,restricted_category:true},
-    {id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'vehicles',name:'VEHICLES',display_order:120,is_active:true,is_assignable:true,restricted_category:false}
+    {id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'vehicles',name:'VEHICLE',display_order:120,is_active:true,is_assignable:true,restricted_category:false},
+    {id:'4b64eec5-cd2b-4302-a244-f100852ccc23',code:'other',name:'OTHER (SPECIFY)',display_order:130,is_active:true,is_assignable:true,restricted_category:false}
   ],
   subcategories: [
     {id:'bbc6f96c-2681-4762-97bd-9bef58e9b062',category_id:'184a9f5f-8870-4112-87e4-d9c8d4c63a5e',code:'ready_made_food',name:'Ready made Food',display_order:10,is_active:true},
     {id:'8e918e4a-b787-417d-b1b8-420543a869b6',category_id:'184a9f5f-8870-4112-87e4-d9c8d4c63a5e',code:'soft_drinks',name:'Soft Drinks',display_order:20,is_active:true},
-    {id:'5392aae9-b04a-47b0-a65a-4e159042de0d',category_id:'184a9f5f-8870-4112-87e4-d9c8d4c63a5e',code:'others',name:'Others',display_order:30,is_active:true},
+    {id:'5392aae9-b04a-47b0-a65a-4e159042de0d',category_id:'184a9f5f-8870-4112-87e4-d9c8d4c63a5e',code:'others',name:'Other (specify)',display_order:30,is_active:true},
 
     {id:'576bfaa4-680c-4305-b325-9ea0911241b7',category_id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'vegetables',name:'Vegetables',display_order:10,is_active:true},
     {id:'84e11b10-aee6-4f54-b3ff-073cfadcefdd',category_id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'fruits',name:'Fruits',display_order:20,is_active:true},
     {id:'34fe6137-012a-481c-85bb-0129a9015e82',category_id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'produce_beef_items',name:'Produce/Beef items (Beef, fish etc)',display_order:30,is_active:true},
     {id:'1886f85d-5779-4cf3-b7e8-eda04e67ba0c',category_id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'herbs_spices',name:'Herbs & Spices',display_order:40,is_active:true},
-    {id:'146d67ad-d0e2-4c50-a693-daa3869ac91d',category_id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'others',name:'Others',display_order:50,is_active:true},
+    {id:'146d67ad-d0e2-4c50-a693-daa3869ac91d',category_id:'b6de14f5-0c81-4da3-95b3-92a5f913fe3d',code:'others',name:'Other (specify)',display_order:50,is_active:true},
 
     {id:'b8e86dd1-1f04-44da-b0bb-79d466e703b2',category_id:'09f6b9b3-32ce-47bf-8b74-1c300d5d405c',code:'otc_medicine',name:'Over the counter medicine',display_order:10,is_active:true},
     {id:'7c5d755c-970b-4287-9e0f-61be52416731',category_id:'09f6b9b3-32ce-47bf-8b74-1c300d5d405c',code:'prescription_medicine',name:'Prescription only Medicine',display_order:20,is_active:true},
@@ -52,21 +53,51 @@ window.LEOGO_PRODUCT_TAXONOMY = {
     {id:'d078b34c-e03c-424a-92f8-41943f682939',category_id:'a7674f9c-0579-42f0-90a5-1ee51fc9727e',code:'plastics',name:'Plastics',display_order:40,is_active:true},
     {id:'10d287f9-7c3a-4a92-93a5-d0a8f2248ce6',category_id:'a7674f9c-0579-42f0-90a5-1ee51fc9727e',code:'hardware',name:'Hardware',display_order:50,is_active:true},
     {id:'0e139bf6-127c-4265-aea2-2d328906ee91',category_id:'a7674f9c-0579-42f0-90a5-1ee51fc9727e',code:'electronics',name:'Electronics',display_order:60,is_active:true},
-    {id:'10708639-7b7e-4d7a-acd9-cf4629eba3a2',category_id:'a7674f9c-0579-42f0-90a5-1ee51fc9727e',code:'others',name:'Others',display_order:70,is_active:true},
+    {id:'10708639-7b7e-4d7a-acd9-cf4629eba3a2',category_id:'a7674f9c-0579-42f0-90a5-1ee51fc9727e',code:'others',name:'Other (specify)',display_order:70,is_active:true},
 
     {id:'6704c2a3-d748-4744-bc33-6c84f77c781b',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'necklaces',name:'Neckles',display_order:10,is_active:true},
     {id:'a282209d-d896-444c-bf3b-df99bdef0f5e',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'perfumes',name:'Perfumes',display_order:20,is_active:true},
     {id:'096a016c-6a36-43c6-a670-703f9edd07b7',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'watches',name:'Watches',display_order:30,is_active:true},
     {id:'d970ec48-f87c-4c35-b3e3-2de689b5b5cf',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'earrings',name:'Earings',display_order:40,is_active:true},
     {id:'7df97a7c-fcc7-41f0-9d7b-91f19210a297',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'bracelets',name:'Bracelets',display_order:50,is_active:true},
-    {id:'02a568e0-c7b3-4e5a-8ed1-4c4842e003b3',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'others',name:'Others',display_order:60,is_active:true},
+    {id:'02a568e0-c7b3-4e5a-8ed1-4c4842e003b3',category_id:'0a9d03d1-a2ca-41da-aaae-d9e0553536cf',code:'others',name:'Other (specify)',display_order:60,is_active:true},
 
     {id:'03a15469-1ecc-4ced-9c3b-ff083f1d6a1f',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'wines',name:'wines',display_order:10,is_active:true},
     {id:'bafb8060-f102-4a14-bf07-531345776ad6',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'beers',name:'Beers',display_order:20,is_active:true},
     {id:'6f004183-0a13-4a87-b6e2-48e5cc266494',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'spirits',name:'Spirit',display_order:30,is_active:true},
     {id:'7f9974d7-6d4d-44ed-a5ee-11421f307f49',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'soft_drinks',name:'Soft Drinks',display_order:40,is_active:true},
     {id:'9bc3812d-c405-47e0-83c8-970cad7190bc',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'energy_drinks',name:'Energy Drinks',display_order:50,is_active:true},
-    {id:'16778d49-191c-461f-bcf2-c813bc7262ea',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'cocktail_mixed_drinks',name:'Cocktail & Mixe Drinks',display_order:60,is_active:true}
+    {id:'16778d49-191c-461f-bcf2-c813bc7262ea',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'cocktail_mixed_drinks',name:'Cocktail & Mixe Drinks',display_order:60,is_active:true},
+    {id:'50f7c3e4-c211-49ab-8807-85be252cc6c1',category_id:'09f6b9b3-32ce-47bf-8b74-1c300d5d405c',code:'others',name:'Other (specify)',display_order:40,is_active:true},
+    {id:'ae1a09bf-d8f4-4d6a-a99e-4f595d03aedb',category_id:'3a035dd7-3760-4a6a-b5e4-230ee40bc179',code:'others',name:'Other (specify)',display_order:60,is_active:true},
+
+    {id:'1ee6d00d-49ab-48bf-beb0-220060b989f6',category_id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'text_books',name:'Text Books',display_order:10,is_active:true},
+    {id:'8901d01a-f2b2-42b2-9544-51f13b911736',category_id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'writing_books',name:'Writing Books',display_order:20,is_active:true},
+    {id:'62f87345-1bfe-44bb-9851-de44eb4216ed',category_id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'pens',name:'Pens',display_order:30,is_active:true},
+    {id:'f5dfa673-34c8-4942-9d2b-7d84c279d34c',category_id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'storybooks',name:'Storybooks',display_order:40,is_active:true},
+    {id:'bb6ac211-0c52-45ce-9f76-7ac2858c987a',category_id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'exam_papers',name:'Exam papers',display_order:50,is_active:true},
+    {id:'a21c0b61-2433-4e94-88e2-2446bd88f7ed',category_id:'b134ae00-e5f1-4903-803b-4093deb83980',code:'others',name:'Other (specify)',display_order:60,is_active:true},
+
+    {id:'ed6f458e-de4b-484e-9da7-47c831f90e2d',category_id:'15774c65-7253-42e6-97fd-8fa46af43d4c',code:'others',name:'Other (specify)',display_order:120,is_active:true},
+
+    {id:'c26c6e7d-396c-408e-83b1-c0cba34062e0',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'t_shirts',name:'T-shirts',display_order:10,is_active:true},
+    {id:'00ce02ee-6233-409d-bad1-3456c9133cdd',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'trousers',name:'Trousers',display_order:20,is_active:true},
+    {id:'4fb44183-6467-48ca-9ab9-9ad1e3fd9b2f',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'shirts',name:'Shirts',display_order:30,is_active:true},
+    {id:'5ac34b8f-72f1-4880-af1e-6b5df54a10c5',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'dresses',name:'Dresses',display_order:40,is_active:true},
+    {id:'aeb87fc1-674a-478d-a0ad-c66a5d8726c2',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'plain_materials',name:'Plain materials',display_order:50,is_active:true},
+    {id:'3f5507ec-ad7f-4b40-bd65-ab4d4106b358',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'uniforms',name:'Uniforms',display_order:60,is_active:true},
+    {id:'3d540ca0-45cb-4319-b4ac-ff4870eed5db',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'footwears',name:'Footwears',display_order:70,is_active:true},
+    {id:'3b9d5ea0-1652-4d9e-afcc-e892f5617e6f',category_id:'f5c1bf6d-3b41-46f2-aa01-e9bb0eab71f3',code:'others',name:'Other (specify)',display_order:80,is_active:true},
+
+    {id:'1fd15e3c-2d16-41e4-90b5-b5f26ec03031',category_id:'f72097df-7436-4558-a8ac-b9eacfabf11d',code:'others',name:'Other (specify)',display_order:70,is_active:true},
+
+    {id:'eda8928d-a1f1-4840-a31a-db6ff3b59dfa',category_id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'motorcycle',name:'Motorcycle',display_order:10,is_active:true},
+    {id:'d1778735-d66f-411e-a83d-74a1216001c0',category_id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'vans_cars',name:'Van/Cars',display_order:20,is_active:true},
+    {id:'d120e9d9-b301-47ed-bbc1-44251534689e',category_id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'pickups',name:'Pickups',display_order:30,is_active:true},
+    {id:'f07db86b-bbfc-467c-8823-02f278af5e98',category_id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'lorries_canters',name:'Lorries/Canters',display_order:40,is_active:true},
+    {id:'5f6e5657-4829-4071-bc97-c6589fcffe63',category_id:'393e3e96-93ee-47ed-a637-b4f2fc412100',code:'others',name:'Other (specify)',display_order:50,is_active:true},
+
+    {id:'4af478d2-c14d-483c-8a46-055901b26bb1',category_id:'4b64eec5-cd2b-4302-a244-f100852ccc23',code:'others',name:'Other (specify)',display_order:10,is_active:true}
   ]
 };
 
